57698583685a02085874824bff1ee02e5349c805
Louys <louysdev@gmail.com>
2022-09-20 11:30:08 -0400

    Booleans

---
+++ js/08.js
@@ -0,0 +1,10 @@
+let nombre = "pepe"
+let email = "pepe@gmail.com"
+
+// Concatenacion 
+console.log(nombre + " " + email)
+
+// Template strings - Strings literals
+
+console.log(`Nombre cliente: ${nombre} || Correo: ${email}`)
+
---
+++ js/09.js
@@ -0,0 +1,11 @@
+// Boolenaos
+
+let boolean1 = true
+let boolean2 = false
+
+console.log(typeof boolean1)
+
+let boolean3 = new Boolean(true);
+
+console.log(boolean3)
+
