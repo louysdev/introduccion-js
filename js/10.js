42da9b68661f750c11915ee9f4c86bbbb0658aec
Louys <louysdev@gmail.com>
2022-09-20 11:38:19 -0400

    Objetos

---
+++ js/10.js
@@ -0,0 +1,13 @@
+// Objetos
+
+const producto = {
+    nombreProducto : "Iphone 10",
+    precio : 1000,
+    disponible : true
+}
+
+console.log(producto)
+
+console.log(producto.precio)
+
+console.log(producto["disponible"])
