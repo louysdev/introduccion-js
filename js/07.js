28c7e48b644b59e19908abd82dcc37470f840e4d
Louys <louysdev@gmail.com>
2022-09-20 11:20:36 -0400

    Controlando el orden de las operaciones

---
+++ js/07.js
@@ -0,0 +1,22 @@
+// Orden de las operaciones
+
+let resultado
+
+resultado = (20 + 1) * 2
+
+resultado = (100 + 200 + 500) * 0.2;
+
+// console.log(resultado)
+
+// Incrementos
+
+let puntaje = 10
+
+puntaje++ // Primero muestra el valor y luego incrementa
+++puntaje // Incrementa y luego muestra el valor
+puntaje--
+--puntaje
+
+puntaje += 10
+
+console.log(puntaje)
