0aee273ec4b2828d0b3f2322ea1bf047c14d3d8c
Louys <louysdev@gmail.com>
2022-09-20 23:42:04 -0400

    Diferencia de un metodo y una funcion

--- js/16.js
+++ js/16.js
@@ -1,20 +1,22 @@
 // Funciones
 
-// Declaracion de function
+// Declaracion de function 
+// Este no da error porque no importa en que lugar se manda a llamar
 
+sumar()
 function sumar() {
     console.log(10 + 10)
 }
 
-sumar()
+
 
 // Expresion de la funcion
-
+// Este da error porque no encuentra la funcion, ya que esta declarada como una variable
+restar();
 const restar = function() {
     console.log(10 - 5);
 }
 
-restar();
     
 // IIFE previene que las variables salgan del archivo
 (function() {
---
+++ js/17.js
@@ -0,0 +1,5 @@
+const numero1 = 20;
+const numero2 = "20"
+
+console.log(parseInt(numero2)) // Es una funcion
+console.log(numero1.toString()) // Es un metodo, ya que va con un . y despues de un elemento
