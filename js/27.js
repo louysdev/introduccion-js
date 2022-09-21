ed328d90ab86c2f4e6d1e9bb43bbfc89b99331a3
Louys <louysdev@gmail.com>
2022-09-21 17:31:15 -0400

    Try catch

---
+++ js/27.js
@@ -0,0 +1,14 @@
+// Try Catch
+
+const numero1 = 20;
+const numero2 = 30;
+
+console.log(numero1)
+
+// Su funcion es ejecutar el codigo y prevernir que por un error la aplicacion se detenga
+try {
+    console.log(numero3)
+} catch (error) {
+    console.log(error)
+}
+console.log(numero2)
