7cb30411a9746110710bd6ff713548c3257425cf
Louys <louysdev@gmail.com>
2022-09-21 13:04:26 -0400

    Herencia en JS

--- js/26.js
+++ js/26.js
@@ -19,3 +19,19 @@ const producto = new Producto("Iphone 14", 1100)
 console.log(producto.precioProducto())
 
 console.log(producto)
+
+// Herencia
+class Libro extends Producto{ //Usa los elementos de la clase padre
+    constructor(nombre, precio, isbn) {
+        super(nombre, precio); //Llama las propiedades del constructor padre
+        this.isbn = isbn
+    }
+
+    formatearProducto() {
+        return `${super.formatearProducto()} | ISBN: ${this.isbn}`;
+    }
+}
+
+const libro = new Libro('JS hoy en dia', 100, "asddfasdassda");
+console.log(libro.formatearProducto())
+
