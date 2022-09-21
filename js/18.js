bf37e6342f41203198714b33925a2990bb3ec6ce
Louys <louysdev@gmail.com>
2022-09-20 23:51:10 -0400

    Funciones con parametros y argumentos

---
+++ js/18.js
@@ -0,0 +1,11 @@
+
+function sumar(numero1, numero2) {
+    console.log(numero1 + numero2)
+}
+sumar(1, 2)
+
+
+const restar = function(numero1 = 0, numero2 = 0) { // Con parametros por defecto
+    console.log(numero1 - numero2);
+}
+restar(23);
